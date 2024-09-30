ed2d95d31a2fea95f7e2745b0fe404da31e5c875
Geovanni <carbajal.perez180320@gmail.com>
2024-09-30 15:45:57 -0600

    Creacion del primer componente de inicio y prueba de Bulma

---
+++ src/Componentes/Inicio.jsx
@@ -0,0 +1,20 @@
+import imgLogo from '../Imagenes/ImagenDemon.png'
+
+const Inicio = () => {
+    return (
+        <>
+            <div className = "">
+                <div className='img'>
+                    <img src={imgLogo} width="200px" />
+                </div>
+                <p class="has-text-danger-35">Quiz Demon</p>
+                Quiz Demon
+                <br />
+                texto raro solo para describir por el momentos todo el proyecto que estoy realizando
+
+            </div>
+        </>
+    );
+}
+
+export default Inicio;
