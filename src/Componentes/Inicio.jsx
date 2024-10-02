51b3586e9f08b58c128e3ca4b4265cb83618373a
Geovanni <carbajal.perez180320@gmail.com>
2024-10-02 15:05:58 -0600

    Edicion del componente Inicio

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -3,14 +3,13 @@ import imgLogo from '../Imagenes/ImagenDemon.png'
 const Inicio = () => {
     return (
         <>
-            <div className = "">
-                <div className='img'>
+            <div className="box ">
+                <div className='card has-background-white'>
                     <img src={imgLogo} width="200px" />
+                    <h1 className='has-text-danger-dark is-size-1 mt-1 m-2'>Quiz Demon</h1>
+                    <p className='has-text-primary-dark is-size-5'> texto raro solo para describir por el momentos todo el proyecto que estoy realizando</p>
+
                 </div>
-                <p class="has-text-danger-35">Quiz Demon</p>
-                Quiz Demon
-                <br />
-                texto raro solo para describir por el momentos todo el proyecto que estoy realizando
 
             </div>
         </>
