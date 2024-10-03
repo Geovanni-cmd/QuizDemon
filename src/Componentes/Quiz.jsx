f4548c6a436a4ba55355df4c7cc90ae3dd8a0497
Geovanni <carbajal.perez180320@gmail.com>
2024-10-03 14:42:12 -0600

    Agrego las imagenes y la funcionalidad del seguimiento de las preguntas

--- src/Componentes/Quiz.jsx
+++ src/Componentes/Quiz.jsx
@@ -1,36 +1,135 @@
+import { useState } from 'react';
 import img_1 from '../Imagenes/Tanjiro.png'
+import img_2 from '../Imagenes/Respiraciones.png'
+import img_3 from '../Imagenes/Nezuko.png'
+import img_4 from '../Imagenes/Muzan.png'
+import img_5 from '../Imagenes/Nezuko_1.png'
+import img_6 from '../Imagenes/Jigoro.png'
+import img_7 from '../Imagenes/Kyogai.png'
+import img_8 from '../Imagenes/Inosuke.png'
+import img_9 from '../Imagenes/Urokodaki.png'
+
 
 const Quiz = () => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // Índice de la pregunta actual
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const preguntas = [
+        {
+            id: 1,
+            pregunta: "¿Cuál es el nombre del protagonista de Kimetsu no Yaiba?",
+            opciones: ["A) Zenitsu Agatsuma", "B) Tanjiro Kamado", "C) Inosuke Hashibira", "D) Giyu Tomioka"],
+            respuestaCorrecta: "B) Tanjiro Kamado",
+            imagen: img_1,
+        },
+        {
+            id: 2,
+            pregunta: "¿Cuál es la respiración que utiliza Tanjiro?",
+            opciones: ["A) Respiración de la Roca", "B) Respiración del Agua", "C) Respiración de la Bestia", "D) Respiración del Trueno"],
+            respuestaCorrecta: "B) Respiración del Agua",
+            imagen: img_2,
+        },
+        {
+            id: 3,
+            pregunta: "¿Qué convierte a Nezuko en un demonio?",
+            opciones: ["A) Un demonio la ataca", "B) Se expone a la luz de la luna", "C) Toma una poción maldita", "D) Es mordida por otro demonio"],
+            respuestaCorrecta: "A) Un demonio la ataca",
+            imagen: img_3,
+        },
+        {
+            id: 4,
+            pregunta: "¿Cómo se llama el villano principal de Kimetsu no Yaiba?",
+            opciones: ["A) Kibutsuji Muzan", "B) Akaza", "C) Enmu", "D) Gyutaro"],
+            respuestaCorrecta: "A) Kibutsuji Muzan",
+            imagen: img_4,
+        },
+        {
+            id: 5,
+            pregunta: "¿Cuál es la habilidad especial de Nezuko como demonio?",
+            opciones: ["A) Puede volar", "B) Puede controlar el fuego", "C) Puede reducir su tamaño", "D) Puede regenerarse rápidamente"],
+            respuestaCorrecta: "C) Puede reducir su tamaño",
+            imagen: img_5,
+        },
+        {
+            id: 6,
+            pregunta: "¿Cuál es el nombre del Pilar del Trueno?",
+            opciones: ["A) Kyojuro Rengoku", "B) Jigoro Kuwajima", "C) Sanemi Shinazugawa", "D) Kaigaku"],
+            respuestaCorrecta: "D) Jigoro Kuwajima",
+            imagen: img_6,
+        },
+        {
+            id: 7,
+            pregunta: "¿Qué demonio derrotó Tanjiro en su primer combate real?",
+            opciones: ["A) Rui", "B) Sabito", "C) Kyogai", "D) Tamayo"],
+            respuestaCorrecta: "C) Kyogai",
+            imagen: img_7,
+        },
+        {
+            id: 8,
+            pregunta: "¿Qué caracteriza a Inosuke Hashibira?",
+            opciones: ["A) Es tímido y callado", "B) Siempre usa una máscara de jabalí", "C) Usa la Respiración del Trueno", "D) Es el hermano de Zenitsu"],
+            respuestaCorrecta: "B) Siempre usa una máscara de jabalí",
+            imagen: img_8,
+        },
+        {
+            id: 9,
+            pregunta: "¿Quién es el mentor de Tanjiro?",
+            opciones: ["A) Kyojuro Rengoku", "B) Urokodaki Sakonji", "C) Giyu Tomioka", "D) Sabito"],
+            respuestaCorrecta: "B) Urokodaki Sakonji",
+            imagen: img_9,
+        },
+        {
+            id: 10,
+            pregunta: "¿Cuál es el objetivo principal de Tanjiro en la serie?",
+            opciones: ["A) Convertirse en el Pilar del Fuego", "B) Derrotar a Kibutsuji Muzan", "C) Encontrar a su padre", "D) Curar a su hermana Nezuko"],
+            respuestaCorrecta: "D) Curar a su hermana Nezuko",
+            imagen: img_1,
+        },
+    ];
+
+
+    const handleAnswer = (answer) => {
+        setUserAnswers([...userAnswers, answer]);
+        //Ir a la soiguiente pregunta si no es la ultima
+        if (currentQuestionIndex < preguntas.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1)
+        } else {
+            //Mostrar resultados del quiz al finalizar
+            console.log("Quiz terminado");
+
+        }
+    }
+
+
     return (
         <div>
             <div className="box">
                 <div className='card has-background-white mt-1'>
-                    
+
                     <div className="columns">
-                    <div className="column">
+                        <div className="column">
+
                             <div className="pregunta card">
-                                ¿Cuál es el nombre del protagonista de Kimetsu no Yaiba?
-                                <div className="columns mt-3">
-                                    <div className="column">
-                                        <button className="button">A) Zenitsu Agatsuma</button>
+                                {preguntas[currentQuestionIndex].id} {preguntas[currentQuestionIndex].pregunta}
+
+                            </div>
+                            <div className='card'>
+                                {preguntas[currentQuestionIndex].opciones.map((opcion, index) => (
+                                    <div className='columns'>
+                                        <div className='column'>
+                                            <button className='button' key={index} onClick={() => handleAnswer(opcion)}>
+                                                {opcion}
+                                            </button>
+                                        </div>
                                     </div>
-                                    <div className="column">
-                                        <button className="button">B) Tanjiro Kamado</button>
-                                    </div>
-                                </div>
-                                <div className="columns">
-                                    <div className="column">
-                                        <button className="button">C) Inosuke Hashibira</button>
-                                    </div>
-                                    <div className="column">
-                                        <button className="button">D) Giyu Tomioka</button>
-                                    </div>
-                                </div>
+
+                                ))}
                             </div>
                         </div>
                         <div className=' '>
                             <div className="column">
-                                <img src={img_1} width='65%' />
+                                <img src={preguntas[currentQuestionIndex].imagen} width='65%' />
+                               
                             </div>
                         </div>
                     </div>
