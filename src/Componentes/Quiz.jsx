32dd3e6383cd5fe6cb17e38c78ed65ef1a468710
Geovanni <carbajal.perez180320@gmail.com>
2024-10-03 00:04:59 -0600

    Implementacion de prueba de imagen y texto inicial de la preguntas

--- src/Componentes/Quiz.jsx
+++ src/Componentes/Quiz.jsx
@@ -1,27 +1,36 @@
+import img_1 from '../Imagenes/Tanjiro.png'
+
 const Quiz = () => {
     return (
         <div>
             <div className="box">
                 <div className='card has-background-white mt-1'>
+                    
                     <div className="columns">
-                        <div className="column">
-                            <img src="" alt="Oli" />
-                        </div>
-                        <div className="column">
+                    <div className="column">
                             <div className="pregunta card">
-                                Primera preguntas
-                            </div>
-                            <div className="opciones card">
-                                <div className="columns">
+                                ¿Cuál es el nombre del protagonista de Kimetsu no Yaiba?
+                                <div className="columns mt-3">
                                     <div className="column">
-                                        <button className="button">a</button>
-                                        <button className="button">b</button>
+                                        <button className="button">A) Zenitsu Agatsuma</button>
                                     </div>
                                     <div className="column">
-                                        <button className="button">c</button>
-                                        <button className="button">d</button>
+                                        <button className="button">B) Tanjiro Kamado</button>
                                     </div>
                                 </div>
+                                <div className="columns">
+                                    <div className="column">
+                                        <button className="button">C) Inosuke Hashibira</button>
+                                    </div>
+                                    <div className="column">
+                                        <button className="button">D) Giyu Tomioka</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=' '>
+                            <div className="column">
+                                <img src={img_1} width='65%' />
                             </div>
                         </div>
                     </div>
