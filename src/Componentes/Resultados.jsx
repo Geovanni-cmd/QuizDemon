d75dee59793e5e0950721535cd6273195ec30705
Geovanni <carbajal.perez180320@gmail.com>
2024-10-16 14:04:15 -0600

    Diseño del componente de Resultados

--- src/Componentes/Resultados.jsx
+++ src/Componentes/Resultados.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Resultados = () => {
     const location = useLocation();
     const { preguntas, userAnswers } = location.state; // Obtener las preguntas y respuestas
+    
+    // Contadores de respuestas correctas e incorrectas
+    let correctas = 0;
+    let incorrectas =0;
 
     return (
         <div>
-            <div className='card has-background-white   '>
+            <div className='card content has-background-white   '>
                 <h1 className='is-size-2 has-text-black'>Resultados del Quiz</h1>
-                
+                <div className='content mt-4'>
+
+                    {preguntas.map((pregunta, index) => (
+                        <div key={index}>
+                            <div className={userAnswers[index] == pregunta.respuestaCorrecta ? "card has-background-primary mt-4" : "card has-background-danger mt-4"}>
+                                <div className='column'>
+                                    <h3 className='is-size-6 has-text-black'>{pregunta.pregunta}</h3>
+                                    <p className='is size-4 has-text-black'>Tu respuesta: <br/> {userAnswers[index]}</p>
+                                    <p> {userAnswers[index] == pregunta.respuestaCorrecta ? "CORRECTO" : "INCORRECTO"}</p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    ))}
+                </div>
+
 
             </div>
 
-            {preguntas.map((pregunta, index) => (
-                <div key={index}>
-                    <h3>{pregunta.pregunta}</h3>
-                    <p>Tu respuesta: {userAnswers[index]}</p>
-                    <p>Respuesta correcta: {pregunta.respuestaCorrecta}</p>
-                </div>
-            ))}
+
         </div>
     );
 }
